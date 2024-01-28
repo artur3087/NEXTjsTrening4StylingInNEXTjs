dc7abc17c4c3362e652255615e7842435e4e18a7
Artur <artur3087@gmail.com>
2024-01-28 16:29:59 +0000

    first commit

---
+++ src/app/About/page.js
@@ -0,0 +1,16 @@
+
+export default function AboutPage () { // START of a default function 
+
+    return ( // RETURN START //
+    
+        <div>
+            <h2>ABOUT</h2>
+            <p>About page, dude! About!</p>
+        </div>
+    
+    
+    ) // RETURN END //
+
+
+
+} // END of a default function //
